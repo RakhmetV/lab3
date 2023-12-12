06b8c551584882df73b240f574809400ee1ef6b6
RakhmetV <atheist01@mail.ru>
2023-12-12 21:28:53 +0500

    Revert "reset com"
    This reverts commit 67ad5a57b29c867dc43d3d43bc2460c28a81f908.

--- main.cpp
+++ main.cpp
@@ -2,11 +2,7 @@
 // Rakhmetov Vadim
 int main(int argc, char argv)
 {
-<<<<<<< HEAD
 char first[255], last[255]; // new izmenenie
-=======
-char first[255], last[255];
->>>>>>> parent of 26df05d (3 commit)
 printf(&quot;Enter your first name: &quot;);
 fgets(first, 255, stdin);
 first[strlen(first)-1] = &#39;\0&#39;; /* remove the newline at the end */
@@ -14,6 +10,6 @@ printf(&quot;Now enter your last name: &quot;);
 gets(last); /* buffer overflow? what&#39;s that? */
 printf(&quot;Hello %s %s!\n&quot;, first, last);
 return 0;
-//new comment
+
 //my master change
 }
